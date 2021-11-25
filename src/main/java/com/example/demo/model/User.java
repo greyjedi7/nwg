b2931541ce2fa5e65b9c2d24e468d6c7cbfdf832
greyjedi7 <karthigeyanmsd@gmail.com>
2021-11-25 12:14:06 +0530

    New Change

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class User {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userid;
 	private String name;
 	private String mail;
--- src/main/java/com/example/demo/repository/TransactionRepository.java
+++ src/main/java/com/example/demo/repository/TransactionRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.Transaction;
 import com.example.demo.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
-	Optional<Transaction> getId(String counterParty, String party)
-			;
+	Optional<List<Transaction>> findByPartyAndCounterParty(String party, String CounterParty);
 }
